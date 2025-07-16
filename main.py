248f0ce8c4ae62cf6e1d6a6e416ff7903cfe8b59
Akshay Lonkar <akshaylonkar2004@gmail.com>
2025-07-16 03:19:46 +0100

    implemented mean function change

--- main.py
+++ main.py
@@ -16,14 +16,15 @@ import gpflow
 
 class RevisedMeanFunction(gpflow.functions.MeanFunction):
 
-    def __init__(self, X, Y):
+    def __init__(self, function_data=None):
         super().__init__()
-        self.mapping = dict(zip(X, Y))
+        self.current_model = function_data
 
     def __call__(self, x):
-        x_values = x.numpy().flatten()
-        y = np.vectorize(self.mapping.get)(x_values)
-        return tf.convert_to_tensor(y.reshape(-1, 1), dtype=tf.float64)
+        if self.current_model is None:
+            return tf.zeros_like(x, dtype=tf.float64)
+        mean_values, _ = self.current_model.predict_f(x)
+        return mean_values
     
 class ResultsObject:
 
@@ -62,12 +63,13 @@ def run_walkforward_analysis(data):
         data=(tf.convert_to_tensor(np.zeros((1, 1)), dtype=tf.float64),
               tf.convert_to_tensor(np.zeros((1, 1)), dtype=tf.float64)), 
         kernel=gpflow.kernels.Matern32(),
-        mean_function=gpflow.mean_functions.Constant(0.0)
+        mean_function=RevisedMeanFunction()
     )
     current_X, current_Y = None, None
     prediction_window = None
     optim = gpflow.optimizers.Scipy()
-    for i in range(5):
+    print("Starting analysis...")
+    for i in range(len(data)):
         # Prepare training data for current iteration
         current_X = tf.convert_to_tensor(np.arange(10*i, (10*i)+50).reshape(-1, 1), dtype=tf.float64)
         current_Y = tf.convert_to_tensor(data.training_data[i].reshape(-1, 1), dtype=tf.float64)
@@ -75,17 +77,16 @@ def run_walkforward_analysis(data):
         current_model.data = (current_X, current_Y)
 
         # Train model and get predictions
-        prediction_window = tf.convert_to_tensor(np.arange((10*i)+50, (10*i)+110).reshape(-1, 1), dtype=tf.float64)
+        prediction_window = tf.convert_to_tensor(np.arange((10*i)+50, (10*i)+60).reshape(-1, 1), dtype=tf.float64)
         optim.minimize(current_model.training_loss, current_model.trainable_variables)
         f_mean, f_var = current_model.predict_f(prediction_window)
 
         # Update model performance
-        walk_forward_results.update_predictions(f_mean.numpy().flatten()[:10], f_var.numpy().flatten()[:10])
-        walk_forward_results.update_loss_profile(data.test_data[i], f_mean.numpy().flatten()[:10])
+        walk_forward_results.update_predictions(f_mean.numpy().flatten(), f_var.numpy().flatten())
+        walk_forward_results.update_loss_profile(data.test_data[i], f_mean.numpy().flatten())
 
         # change mean function for next iteration
-        new_mean_function = RevisedMeanFunction(prediction_window.numpy().flatten(), f_mean.numpy().flatten())
-        current_model.mean_function = new_mean_function
+        current_model.mean_function.function_data = current_model
 
     return walk_forward_results
 
@@ -93,4 +94,4 @@ if __name__ == "__main__":
     data = DataSet('EURUSD30.csv')
     results = run_walkforward_analysis(data)
     metrics = results.get_metrics()
-    print(metrics['mean'])
+    print(metrics['loss_profile'])
